fa875eb2f28173349419ab9476290239d3e6cfc7
GitHub Action <action@github.com>
2021-07-02 07:40:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/usnail/easycode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/usnail/easycode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.usnail.easycode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.usnail.easycode.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/usnail/easycode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/usnail/easycode/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.usnail.easycode.services
 
+import com.github.usnail.easycode.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
